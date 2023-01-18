e6b6e53e949ceed00e842728c072b44802e8f4ac
suchartbu <suchartbu@gmail.com>
2023-01-18 13:37:52 +0700

    testing facthExample()

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\UserModel;
 
 class Home extends BaseController
 {
     public function index()
     {
+        $model = new UserModel();
+        print_r($model->facthExample());
+        echo 'testing index';
         return view('welcome_message');
     }
 }
---
+++ app/Models/UserModel.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+/**
+ * Description of UserModel
+ *
+ * @author it
+ */
+class UserModel extends Model {
+
+    //put your code here
+    public function facthExample() {
+        $table = 'aipn_billitems';
+        $an = '652225702';
+        return $this->builder($table)
+                        ->where('AN', $an)
+                        ->get()
+                        ->getResultArray();
+    }
+
+}
