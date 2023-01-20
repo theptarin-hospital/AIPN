ce17981600981f0150b5311728294a821663ef62
suchartbu <suchartbu@gmail.com>
2023-01-20 10:45:04 +0700

    testing AIPNDocument

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,15 +1,19 @@
 <?php
 
 namespace App\Controllers;
-use App\Models\UserModel;
 
-class Home extends BaseController
-{
-    public function index()
-    {
+use App\Models\UserModel;
+use App\Libraries\AIPNDocument;
+
+class Home extends BaseController {
+
+    public function index() {
+        $AIPN = new AIPNDocument();
+        echo 'TEST--->' . $AIPN->sayHi();
         $model = new UserModel();
         print_r($model->facthExample());
         echo 'testing index';
         return view('welcome_message');
     }
+
 }
---
+++ app/Libraries/AIPNDocument.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
+ */
+
+namespace App\Libraries;
+
+/**
+ * Description of AIPNDocument
+ *
+ * @author it
+ */
+class AIPNDocument {
+
+    public function sayHi() {
+        return "AIPN Hello World!";
+    }
+
+}
