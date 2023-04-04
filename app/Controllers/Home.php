7859f294ff266f358be1b66d3977149245ae0095
suchartbu <suchartbu@gmail.com>
2023-04-04 18:59:10 +0700

    สร้างลิงค์สำหรับดาวน์โหลด ZIP สำหรับส่ง สกส. #3

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,15 +7,16 @@ use App\Libraries\Aipn\Aipn;
 class Home extends BaseController {
 
     public function index() {
-        $an = '660175002';
+        $an = '660373007';
         $id = 19999;
+//        $data_ = ['an' => $an, $id => 'id'];
         $aipn = new Aipn($an, $id);
-        echo 'GEN : ' . $an . ' Download -> ' . $aipn->getZipUrl();
+//        echo 'GEN : ' . $an . ' Download -> ' . $aipn->getZipUrl();
 //        echo base_url($aipn->save_zip($id));
 //        echo $aipn->save_zip($id);
 //        echo 'date_default_timezone_get()|' . date_default_timezone_get() . PHP_EOL;
 //        echo 'dapp_timezone()|' . app_timezone() . PHP_EOL;
-        return view('welcome_message');
+        return view('welcome_message', ['an' => $an, $id => 'id', 'url' => $aipn->getZipUrl()]);
     }
 
 }
