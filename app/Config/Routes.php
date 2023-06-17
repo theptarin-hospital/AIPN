9e1765ed13b78b08d2babc20cf51b0692ab098cb
suchartbu <suchartbu@gmail.com>
2023-06-17 17:00:43 +0700

    Create Upload pages.

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -30,6 +30,8 @@ $routes->set404Override();
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('upload', 'Upload::index');          // Add this line.
+$routes->post('upload/upload', 'Upload::upload'); // Add this line.
 
 use App\Controllers\Pages;
 
